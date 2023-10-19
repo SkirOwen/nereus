5279bc47680e173208194acfdc9a5011371806fe
Owen Allemang <owen.allemang@gmail.com>
2023-10-19 15:35:20 +0100

    function to calculate the md5

--- nereus/utils/file_ops.py
+++ nereus/utils/file_ops.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import hashlib
 
 
 def guarantee_existence(path: str) -> str:
@@ -20,3 +21,23 @@ def guarantee_existence(path: str) -> str:
 		os.makedirs(path)
 	return os.path.abspath(path)
 
+
+def calculate_md5(file_path: str) -> str:
+	"""
+	Calculate the MD5 checksum of a file.
+
+	Parameters
+	----------
+	file_path : str
+		The path to the file for which the MD5 checksum is to be calculated.
+
+	Returns
+	-------
+	str
+		The MD5 checksum of the file.
+	"""
+	hash_md5 = hashlib.md5()
+	with open(file_path, "rb") as f:
+		for chunk in iter(lambda: f.read(4096), b""):
+			hash_md5.update(chunk)
+	return hash_md5.hexdigest()
