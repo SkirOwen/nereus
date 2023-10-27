b4fbd7dd0ce99a7c5b3d59c16a7a2b12aed7f9a3
Owen Allemang <owen.allemang@gmail.com>
2023-10-27 14:28:13 +0100

    created skipwise function, similar to itertools' pairwise but can skip some combinations

---
+++ nereus/utils/iterable_ops.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import itertools as it
+
+from typing import Iterable, Iterator
+
+
+def skipwise(
+		iterable: Iterable,
+		step: int,
+		start: None | int = None,
+		stop: None | int = None
+) -> Iterator:
+	pairwise_iter = it.pairwise(iterable)
+	return it.islice(pairwise_iter, start, stop, step)
