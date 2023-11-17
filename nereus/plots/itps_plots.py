5f243545f14de32f0a91662e3e33e2ae4f28bb9a
Owen Allemang <owen.allemang@gmail.com>
2023-11-17 14:38:01 +0000

    plot hist of the time of the itps

--- nereus/plots/itps_plots.py
+++ nereus/plots/itps_plots.py
@@ -15,7 +15,7 @@ from nereus import logger
 def map_itps(itps_lat_lon: list):
 	lat, lon = itps_lat_lon
 
-	fig = plt.figure(figsize=(20, 20))
+	fig = plt.figure(figsize=(10, 10), dpi=300)
 	ax = fig.add_subplot(1, 1, 1, projection=ccrs.NorthPolarStereo())
 	ax.set_extent([-180, 180, 90, 55], ccrs.PlateCarree())
 	theta = np.linspace(0, 2 * np.pi, 100)
@@ -26,6 +26,23 @@ def map_itps(itps_lat_lon: list):
 	ax.set_boundary(circle, transform=ax.transAxes)
 
 	ax.scatter(lon, lat, transform=ccrs.PlateCarree())
+	plt.show()
+
+
+def time_hist(metadatas) -> None:
+	years = pd.to_datetime(metadatas.time).dt.year.values
+	months = pd.to_datetime(metadatas.time).dt.month.values
+
+	fig, axs = plt.subplots(1, 2, figsize=(8, 4), dpi=300)
+	bins_month = np.arange(1, 14)  # set bin edges
+	axs[0].hist(months, bins=bins_month)
+	axs[0].set_xticks([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1])
+	axs[0].set_title('Month')
+	bins_year = np.arange(2004, 2022)  # set bin edges
+	axs[1].hist(years, bins=bins_year)
+	axs[1].set_xticks(bins_year)
+	axs[1].set_xticklabels(bins_year, rotation=90)  # rotate xticklabels by 90 degrees
+	axs[1].set_title('Year')
 	plt.show()
 
 
