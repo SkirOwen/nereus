4b33f2525329d7d8258f3ecc5e45b08340f47228
Owen Allemang <owen.allemang@gmail.com>
2023-11-17 14:43:27 +0000

    starting point for the argo float code handling

---
+++ nereus/datasets/argo.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+
+from nereus import logger
+from nereus.utils.downloader import downloader
+
+URL = "https://data-argo.ifremer.fr"
+
+
+def download_argo():
+	pass
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
