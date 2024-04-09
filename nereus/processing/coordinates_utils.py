171dec7d9704bf4796a87edc1aea38e5d7280d05
Owen A <owen.allemang@gmail.com>
2024-04-10 00:56:28 +0100

    helper function to re-proj and bin the data and [WIP] for finding closest points using a KDTree

--- nereus/processing/coordinates_utils.py
+++ nereus/processing/coordinates_utils.py
@@ -5,6 +5,8 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 
+from scipy.spatial import KDTree
+
 
 def change_coords(data: xr.Dataset, proj):
 	# Create a Plate Carree projection for the original data
@@ -28,6 +30,18 @@ def grid_bin(data: xr.Dataset, x_step: float, y_step: float, scale: float = 1):
 	lat_bins = pd.cut(df_latlon['x'], bins=np.arange(min(df_latlon['x']), max(df_latlon['x']), x_step / scale))
 	latlon_groups = df_latlon.groupby([lon_bins, lat_bins], observed=False)
 	return latlon_groups
+
+
+def grid_bin_coord(data: xr.Dataset, proj, x_step: float, y_step: float, scale: float = 1):
+	data = change_coords(data, proj)
+	latlon_group = grid_bin(data, x_step, y_step, scale)
+	return latlon_group
+
+
+def find_closest(coordinates: list[tuple[float, float]]) -> tuple[np.ndarray, np.ndarray]:
+	tree = KDTree(coordinates)
+	dd, ii = tree.query(coordinates, k=2)
+	return dd, ii
 
 
 def main():
