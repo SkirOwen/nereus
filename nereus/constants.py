7f49e37663e107463bcb008266ae7c667f33c6f3
Owen A <owen.allemang@gmail.com>
2024-04-14 02:15:59 +0100

    adding some colours as constants

---
+++ nereus/constants.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+
+PINKISH = "#cc6677"         # red              pinkish / dusty rose
+SAND = "#ddcc77"
+DARK_GREEN = "#117733"      # dark green        darkish green
+LIGHT_BLUE = "#88ccee"      # light blue
+TEAL = "#44aa99"            # tesl blue         blue-green
+DARK_MAGENTA = "#882255"    # dark magenta     red purple
+DEEP_BLUE = "#332288"       # deep blue        dark royal blue
