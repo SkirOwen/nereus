0900f2f0cccce5c64b1095bf0e6d97dc9d6f8edb
Owen A <owen.allemang@gmail.com>
2024-04-19 12:35:26 +0100

    adding submodules and correct order for the logger

--- nereus/__init__.py
+++ nereus/__init__.py
@@ -1,13 +1,17 @@
+from __future__ import annotations
+
 import logging
 
+# This needs to be at the top as other files depend on it
 from .logger import make_logger
 
 __all__ = [
-	"logger"
-	# "cluster",
-	# "plots",
-	# "processing"
+	"logger",
+	"cluster",
+	"plots",
+	"processing",
 ]
+
 
 logger: logging.Logger
 
