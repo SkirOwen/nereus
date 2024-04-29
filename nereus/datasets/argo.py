b4163f475b948d074bddb49d447fd0b52cdeed3c
Owen A <owen.allemang@gmail.com>
2024-04-29 17:38:46 +0100

    [WIP] making argos parallel

--- nereus/datasets/argo.py
+++ nereus/datasets/argo.py
@@ -5,11 +5,14 @@ import os
 import shutil
 
 from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from typing import Iterable
 
 import numpy as np
 import pandas as pd
 import xarray as xr
 
+from rich.progress import Progress, TaskID
 from tqdm import tqdm
 
 from nereus import logger
@@ -47,7 +50,7 @@ def load_all_argo() -> list:
 	]
 
 	argos = []
-	for f in tqdm(all_argo):
+	for f in tqdm(all_argo, desc="Opening Argos"):
 		argos.append(xr.open_dataset(os.path.join(get_argo_dir(), f)))
 	return argos
 
@@ -98,29 +101,41 @@ def load_all_argo() -> list:
 # 	)
 
 
-def process_argo(argo, x_inter=np.arange(10, 760, 10), base_dim="PRES", dims=("TEMP", "PSAL", "DOX2_ADJUSTED")):
-	processed = []
+def process_argo(
+		argo: xr.Dataset,
+		x_inter: np.ndarray = np.arange(10, 760, 10),
+		base_dim: str = "PRES",
+		dims: Iterable[str] = ("TEMP", "PSAL", "DOX2_ADJUSTED"),
+		task_tot: int | None = None,
+		task_num: int | None = None,
+	) -> list:
+	""""""
+	processed_argo = []
+	with tqdm(total=argo.TIME.size, desc=f"(Task {task_num} / {task_tot}) Process date", position=1, leave=False) as pbar:
+		for i in range(argo.TIME.size):
+			if np.all(np.diff(argo.PRES[i]) > 0):  # Making sure no NaNs in the pressure and strictly increasing
+				continue
+			if (argo.PRES[i].min() > 10.0) | (argo.PRES[i].max() < 750.0) | (len(argo.PRES[i]) <= 2):
+				continue
 
-	for i in range(argo.TIME.size):
-		if (argo.PRES[i].min() > 10.0) | (argo.PRES[i].max() < 750.0) | (len(argo.PRES[i]) <= 2):
-			continue
+			interp_argo = {
+				"time": np.full(x_inter.shape, argo.TIME[i].data),
+				"lat": np.full(x_inter.shape, argo.LATITUDE[i].data),
+				"lon": np.full(x_inter.shape, argo.LONGITUDE[i].data),
+				"profile": np.full(x_inter.shape, f"argo_{argo.id}_{i}"),
+				base_dim: x_inter,
+			}
 
-		interp_argo = {
-			"time": np.full(x_inter.shape, argo.TIME[i].data),
-			"lat": np.full(x_inter.shape, argo.LATITUDE[i].data),
-			"lon": np.full(x_inter.shape, argo.LONGITUDE[i].data),
-			"profile": np.full(x_inter.shape, f"argo_{argo.id}_{i}"),
-			base_dim: x_inter,
-		}
+			for dim in dims:
+				if dim in argo:
+					interp_argo[dim] = np.interp(x_inter, argo[base_dim][i].data, argo[dim][i].data)
+				else:
+					interp_argo[dim] = np.full(x_inter.shape, np.nan)
+			processed_argo.append(pd.DataFrame(interp_argo))
+			pbar.update(1)
+		pbar.close()
 
-		for dim in dims:
-			if dim in argo:
-				interp_argo[dim] = np.interp(x_inter, argo[base_dim][i].data, argo[dim][i].data)
-			else:
-				interp_argo[dim] = np.full(x_inter.shape, np.nan)
-		processed.append(pd.DataFrame(interp_argo))
-
-	return processed
+	return processed_argo
 
 
 def argos_to_xr(argos: pd.DataFrame) -> xr.Dataset:
@@ -136,14 +151,14 @@ def argos_to_xr(argos: pd.DataFrame) -> xr.Dataset:
 def preload_argo(**kwargs) -> str:
 	# check download
 	# parse
-	parallel = False
+	parallel = True
 
 	save_path = os.path.join(get_argo_dir(), "argos_xr.nc")
 
 	if not os.path.exists(save_path):
 		argos = load_all_argo()
 		logger.info("Argo Loaded")
-		processed_argo = []
+		processed_argos = []
 
 		x_inter = np.arange(10, 760, 10)
 		base_dim = "PRES"
@@ -151,34 +166,26 @@ def preload_argo(**kwargs) -> str:
 
 		if parallel:
 			with ProcessPoolExecutor(6) as executor:
-				results = executor.map(process_argo, argos)
-				for result in tqdm(results, total=len(argos)):
-					processed_argo.extend(result)
 
-		for argo in tqdm(argos):
-			for i in range(argo.TIME.size):
-				if np.all(np.diff(argo.PRES[i]) > 0):   # Making sure no NaNs in the pressure and strictly increasing
-					continue
-				if (argo.PRES[i].min() > 10.0) | (argo.PRES[i].max() < 750.0) | (len(argo.PRES[i]) <= 2):
-					continue
+				partial_process = partial(
+					process_argo,
+					x_inter=x_inter,
+					dims=dims,
+					base_dim=base_dim,
+					task_tot = len(argos)
+				)
+				futures = [executor.submit(partial_process, argo, task_num=i) for i, argo in enumerate(argos)]
 
-				interp_argo = {
-					"time": np.full(x_inter.shape, argo.TIME[i].data),
-					"lat": np.full(x_inter.shape, argo.LATITUDE[i].data),
-					"lon": np.full(x_inter.shape, argo.LONGITUDE[i].data),
-					"profile": np.full(x_inter.shape, f"argo_{argo.id}_{i}"),
-					base_dim: x_inter,
-				}
+				for future in tqdm(futures, desc="Processing argos", position=0):
+					processed_argos.extend(future.result())
 
-				for dim in dims:
-					if dim in argo:
-						interp_argo[dim] = np.interp(x_inter, argo[base_dim][i].data, argo[dim][i].data)
-					else:
-						interp_argo[dim] = np.full(x_inter.shape, np.nan)
-				processed_argo.append(pd.DataFrame(interp_argo))
+		else:
+			for argo in tqdm(argos):
+				processed_argo = process_argo(argo, x_inter, base_dim, dims)
+				processed_argos.extend(processed_argo)
 
 		logger.info("Concat")
-		argos = pd.concat(processed_argo, ignore_index=True)
+		argos = pd.concat(processed_argos, ignore_index=True)
 
 		logger.info("Rename")
 		argos.rename(columns=RENAME_COL, inplace=True)
